4218b36348e9691c036777c9a22cd92a4b054cf6
sohamohsen <sohamohsen016@gmai.com>
2026-04-01 22:47:10 +0200

    add backend and frontend structure

---
+++ Smart Grocery BE/src/main/java/com/task/smartgrocerybe/SmartGroceryBeApplication.java
@@ -0,0 +1,13 @@
+package com.task.smartgrocerybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGroceryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGroceryBeApplication.class, args);
+    }
+
+}
